30ddb84500266443cfd9ef353bbe571ddb15236e
durimfetahaj <131853939+durimfetahaj@users.noreply.github.com>
2026-03-03 17:59:53 +0100

    made header sticky and added border bottom

--- components/header.tsx
+++ components/header.tsx
@@ -5,26 +5,28 @@ import { Search } from "./search-bar/search";
 
 export const Header = () => {
   return (
-    <div className="container mx-auto flex flex-wrap items-center gap-4 p-4 py-6 md:justify-between">
-      <div className="order-1">
-        <Logo />
-      </div>
+    <header className="sticky top-0 z-50 bg-white/95 backdrop-blur supports-backdrop-filter:bg-white/80">
+      <div className="container mx-auto mb-10 flex flex-wrap items-center gap-4 p-4 py-6 md:justify-between border-b pb-11">
+        <div className="order-1">
+          <Logo />
+        </div>
 
-      <div className="order-2 ml-auto flex items-center gap-4 lg:order-3 md:ml-0">
-        <div>
-          <Heart />
+        <div className="order-2 ml-auto flex items-center gap-4 lg:order-3 md:ml-0">
+          <div>
+            <Heart />
+          </div>
+          <div>
+            <ShoppingBag />
+          </div>
+          <div>
+            <ProfileView />
+          </div>
         </div>
-        <div>
-          <ShoppingBag />
-        </div>
-        <div>
-          <ProfileView />
-        </div>
-      </div>
 
-      <div className="order-3 basis-full self-center md:mx-auto md:max-w-2xl lg:order-2 lg:basis-auto lg:w-[42rem]">
-        <Search />
+        <div className="order-3 basis-full self-center md:mx-auto md:max-w-2xl lg:order-2 lg:basis-auto lg:w-2xl">
+          <Search />
+        </div>
       </div>
-    </div>
+    </header>
   );
 };
